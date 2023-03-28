aac2987a3d00b69fc66fa5dd0796509c86456ffe
cqs263 <cqs263@hotmail.com>
2023-03-28 16:13:24 +0800

    Mutex方法

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using System.Threading;
+
 namespace XS.JobForCore
 {
     internal static class Program
     {
+        static Mutex mutex = new Mutex(true, "XS.JobForCore");
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -10,8 +13,19 @@ namespace XS.JobForCore
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Main());
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Main());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("只允许运行一个实例！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
